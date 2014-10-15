085e9ff72a79244ddcb50a51e66a3f58f56c3623
Melody Ma <melody.yy.ma@gmail.com>
2014-10-15 01:12:42 +0000

    Updated Artifact model

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -3,7 +3,7 @@ class Artifact < ActiveRecord::Base
   validates :title, presence: true
   validates :country, presence: true
   validates :city, presence: true
-  validates :image, presence: true
+  # validates :image, presence: true
 
   # mount_uploader :image, ImageUploader
 
